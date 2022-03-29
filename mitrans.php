08fd50fe4489dfa1d3140d589caae45ca5b9fee5
webkodetr <57902573+webkodetr@users.noreply.github.com>
2022-03-29 16:42:26 +0800

    Update mitrans.php

--- mitrans.php
+++ mitrans.php
@@ -1,6 +1,6 @@
 <?php
 // Set your server key (Note: Server key for sandbox and production mode are different)
-$server_key = '<server key>';
+$server_key = 'SB-Mid-client-sQRKcfPK-sHoyZtf';
 // Set true for production, set false for sandbox
 $is_production = false;
 
